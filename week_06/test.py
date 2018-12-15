e046989664e54ec41e8b455f620beb048bfb52fe
Илья Голубев <ilya-s-golubev@yandex.ru>
2018-12-15 10:39:08 +0300

    week 06 seminar added

---
+++ week_06/test.py
@@ -0,0 +1,2 @@
+def function_from_my_module(name):
+    print('Hello, {}!'.format(name))
---
+++ week_06/test2.py
@@ -0,0 +1,4 @@
+print('loaded test2.py')
+
+def function_from_my_module2(name):
+    print('HeLlOOo, {}!!!!!!))))))))))'.format(name))
